de2aeee7a7958c9b3e3c93b715ee037ee1d79328
Eduardo Veronez <106968374+eduve@users.noreply.github.com>
2022-06-27 12:22:31 -0300

    Add files via upload

---
+++ exer5.java
@@ -0,0 +1,22 @@
+package Eduardo_e_Gabriel_Firmo;
+
+import java.util.Scanner;
+
+public class exer5 {
+  public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+    int primos, n, i, cont = 0;
+    System.out.println("Digite um número: ");
+    n = entrada.nextInt();
+    for (i = 1; i <= n; i++) {
+      if (n % 1 == 0 && n % i == 0)
+        cont++;
+
+    }
+    if (cont == 2) {
+      System.out.println("O numero é primo");
+    } else {
+      System.out.println("O numero não é primo");
+    }
+  }
+}
