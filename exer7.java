38bb609c582174a40f83d1e48e3cfc905a67e4eb
Eduardo Veronez <106968374+eduve@users.noreply.github.com>
2022-07-08 08:55:56 -0300

    Add files via upload

---
+++ exer7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class exer6 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        float n1
+        System.out.println("Digite as horas trabalhadas: ")
+        n1 = entrada.nextFloat();
+        n1 = (n1 * 10.25);
+        if (n1 < 50){
+            System.out.println("Atenção, dirija-se à direção do Hotel!")
+        }
+    }
+}
